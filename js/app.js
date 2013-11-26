ebbd56faa70ba859097dbc855977dbe77fe95252
Lauriel Mesquita <laurielmesquita@me.com>
2013-11-26 10:50:23 -0300

    Menu nav ready.
    Menu, links and colors link Ok. Brand .SVG and .PNG ready.

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function supportsSVG() {
+    return !! document.createElementNS && !! document.createElementNS('http://www.w3.org/2000/svg','svg').createSVGRect;
+}
+if (!supportsSVG()) {
+    var imgs = document.getElementsByTagName('img');
+    var dotSVG = /.*\.svg$/;
+    for (var i = 0; i != imgs.length; ++i) {
+        if(imgs[i].src.match(dotSVG)) {
+            imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
+        }
+    }
+}
